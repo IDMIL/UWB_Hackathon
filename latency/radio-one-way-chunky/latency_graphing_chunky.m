8a5740ae7886174cdd118a42fff984fafb634dd3
Paul Buser <buser.paul@gmail.com>
2022-11-20 16:39:02 -0500

    Update graphing script with histogram and box plots

--- latency/radio-one-way-chunky/latency_graphing_chunky.m
+++ latency/radio-one-way-chunky/latency_graphing_chunky.m
@@ -1,12 +1,15 @@
 clear;
+close all;
+clc;
+
 x = (1:100);
+
 thirtytwo = readmatrix("result-32-bytes.txt");
 sixtyfour = readmatrix("result-64-bytes.txt");
 onetwentyeight = readmatrix("result-128-bytes.txt");
 twofiftysix = readmatrix("result-256-bytes.txt");
 fivetwelve = readmatrix("result-512-bytes.txt");
 tentwentyone = readmatrix("result-1021-bytes.txt");
-
 
 y_thirtytwo = thirtytwo(x,2);
 y_sixtyfour = sixtyfour(x,2);
@@ -15,7 +18,26 @@ y_twofiftysix = twofiftysix(x,2);
 y_fivetwelve = fivetwelve(x,2);
 y_tentwentyone = tentwentyone(x,2);
 
-plot(x,y_thirtytwo,x,y_sixtyfour,x,y_onetwentyeight,x,y_twofiftysix,x,y_fivetwelve,x,y_tentwentyone)
+data = [y_thirtytwo, y_sixtyfour, y_onetwentyeight, y_twofiftysix, y_fivetwelve, y_tentwentyone];
+
+hold on;
+for i = 1:width(data);
+    histogram(data(:, i), "EdgeColor", "none");
+end
+hold off;
+xlabel('latency in microseconds')
+ylabel('number of samples')
+leg = legend('32', '64', '128', '256', '512', '1021');
+title(leg, "Message Size (bytes)");
+
+figure;
+
+hold on;
+boxplot(data, [32, 64, 128, 256, 512, 1021]);
+% boxplot(data, ["32", "64", "128", "256", "512", "1021"]);
+hold off;
+
+set(gca, 'YScale', 'log')
 title('radio-one-way-chunky')
-xlabel('X')
-ylabel('latency in microseconds')
+xlabel('message size (bytes)')
+ylabel('latency (microseconds)')
